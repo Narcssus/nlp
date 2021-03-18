52c8d9c6251f3db162ad1e9c0853a6d66b3dc8db
Narcssus <narcssusinnook@163.com>
2021-03-18 22:49:43 +0800

    增加返回字段

--- src/main/java/com/narc/nlp/service/nlp/service/impl/TimeNlpServiceImpl.java
+++ src/main/java/com/narc/nlp/service/nlp/service/impl/TimeNlpServiceImpl.java
@@ -3,6 +3,7 @@ package com.narc.nlp.service.nlp.service.impl;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.narc.nlp.service.nlp.service.TimeNlpService;
+import com.time.nlp.StringPreHandlingModule;
 import com.time.nlp.TimeNormalizer;
 import com.time.nlp.TimeUnit;
 import com.time.util.DateUtil;
@@ -36,6 +37,7 @@ public class TimeNlpServiceImpl implements TimeNlpService {
             timeList.add(object);
         }
         res.put("timeList", timeList);
+        res.put("formatStr", StringPreHandlingModule.numberTranslator(str));
         return res;
     }
 }
